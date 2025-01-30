d6bb28f4b847867cca97863400ac5a921b8a5fc7
Kier <kieransmozer@gmail.com>
2025-01-29 22:09:19 -0500

    Underline effect on 'Activate card' Cards & updating snapshots

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -14,7 +14,7 @@ const Card = ({ allowance }: CardProps) => {
 
   return (
     <div
-      className="bg-white rounded-lg p-6 border border-[#DDDDDD] shadow-sm min-h-[185px] flex flex-col justify-between
+      className="group bg-white rounded-lg p-6 border border-[#DDDDDD] shadow-sm min-h-[185px] flex flex-col justify-between
              hover:shadow-md hover:scale-105 transition-transform duration-300"
       data-testid="allowance-card"
     >
@@ -55,8 +55,11 @@ const Card = ({ allowance }: CardProps) => {
       ) : (
         <div className="-mx-6 -mb-6">
           <div className="bg-[#FAFAFA] p-4 rounded-b-lg">
-            <button className={`${inter.className} text-[#569F6E] text-sm`}>
+            <button
+              className={`${inter.className} text-[#569F6E] text-sm relative`}
+            >
               Activate card
+              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#569F6E] transition-all duration-300 group-hover:w-full"></span>
             </button>
           </div>
         </div>
