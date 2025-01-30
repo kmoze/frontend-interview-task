c09a68f0c8437898606cd3808823145a9c1515b1
Kier <kieransmozer@gmail.com>
2025-01-30 00:29:24 -0500

    Adding card BG to Colours object & updating snapshots

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,13 +9,16 @@ type CardProps = {
 }
 
 const COLOURS = {
+  cardBg: 'bg-white',
   primaryText: 'text-[#232323]',
   secondaryText: 'text-[#797979]',
   amountText: 'text-[#9A9A9A]',
   border: 'border-[#DDDDDD]',
+  progressBarFill: 'bg-[#DDDDDD]',
   progressBar: 'bg-[#64D98A]',
   activateBg: 'bg-[#FAFAFA]',
   activateText: 'text-[#569F6E]',
+  activeHover: 'bg-[#569F6E]',
 }
 
 const Card = ({ allowance }: CardProps) => {
@@ -24,7 +27,7 @@ const Card = ({ allowance }: CardProps) => {
 
   return (
     <div
-      className={`${inter.className} group bg-white rounded-lg p-6 border ${COLOURS.border} shadow-sm min-h-[185px] flex flex-col justify-between
+      className={`${inter.className} group ${COLOURS.cardBg} rounded-lg p-6 border ${COLOURS.border} shadow-sm min-h-[185px] flex flex-col justify-between
              hover:shadow-md hover:scale-105 transition-transform duration-300`}
       data-testid="allowance-card"
     >
@@ -50,7 +53,7 @@ const Card = ({ allowance }: CardProps) => {
                 allowance.renewal.slice(1)}
             </span>
           </div>
-          <div className="w-full h-1 bg-[#DDDDDD] rounded-full">
+          <div className={`${COLOURS.progressBarFill} w-full h-1 rounded-full`}>
             <div
               className={`${COLOURS.progressBar} h-full rounded-full`}
               style={{ width: `${spentPercentage}%` }}
@@ -61,9 +64,11 @@ const Card = ({ allowance }: CardProps) => {
       ) : (
         <div className="-mx-6 -mb-6">
           <div className={`${COLOURS.activateBg} p-4 rounded-b-lg`}>
-            <button className={`${COLOURS.activateBg} text-sm relative`}>
+            <button className={`${COLOURS.activateText} text-sm relative`}>
               Activate card
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#569F6E] transition-all duration-300 group-hover:w-full"></span>
+              <span
+                className={`${COLOURS.activeHover} absolute left-0 bottom-0 w-0 h-[2px] transition-all duration-300 group-hover:w-full`}
+              ></span>
             </button>
           </div>
         </div>
