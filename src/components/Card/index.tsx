b08518aece6809306e4cdae8dbe79f5af01398fb
Kier <kieransmozer@gmail.com>
2025-01-29 23:49:12 -0500

    Refactoring font family to be only on root div of Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -14,17 +14,15 @@ const Card = ({ allowance }: CardProps) => {
 
   return (
     <div
-      className="group bg-white rounded-lg p-6 border border-[#DDDDDD] shadow-sm min-h-[185px] flex flex-col justify-between
-             hover:shadow-md hover:scale-105 transition-transform duration-300"
+      className={`${inter.className} group bg-white rounded-lg p-6 border border-[#DDDDDD] shadow-sm min-h-[185px] flex flex-col justify-between
+             hover:shadow-md hover:scale-105 transition-transform duration-300`}
       data-testid="allowance-card"
     >
       <div className="space-y-1.5">
-        <h3
-          className={`${inter.className} text-base font-medium text-[#232323]`}
-        >
+        <h3 className="text-base font-medium text-[#232323]">
           {allowance.name}
         </h3>
-        <p className={`${inter.className} text-sm text-[#797979] font-normal`}>
+        <p className="text-sm text-[#797979] font-normal">
           {allowance.type === 'card' ? 'Spend Card' : 'Expense'}
         </p>
       </div>
@@ -32,12 +30,10 @@ const Card = ({ allowance }: CardProps) => {
       {allowance.active ? (
         <div className="space-y-[-0.25rem]">
           <div className="flex justify-between mb-2">
-            <span
-              className={`${inter.className} text-[#232323] text-sm font-medium`}
-            >
+            <span className="text-[#232323] text-sm font-medium">
               {Math.round(spentPercentage)}% utilised
             </span>
-            <span className={`${inter.className} text-[#9A9A9A] text-sm`}>
+            <span className="text-[#9A9A9A] text-sm">
               {allowance.currency}
               {allowance.amount} /{' '}
               {allowance.renewal.charAt(0).toUpperCase() +
@@ -55,9 +51,7 @@ const Card = ({ allowance }: CardProps) => {
       ) : (
         <div className="-mx-6 -mb-6">
           <div className="bg-[#FAFAFA] p-4 rounded-b-lg">
-            <button
-              className={`${inter.className} text-[#569F6E] text-sm relative`}
-            >
+            <button className="text-[#569F6E] text-sm relative">
               Activate card
               <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-[#569F6E] transition-all duration-300 group-hover:w-full"></span>
             </button>
