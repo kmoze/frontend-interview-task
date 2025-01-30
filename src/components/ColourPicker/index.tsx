376f212b26cf00f8f62f4dd63d7bc93ddfea542a
Kier <kieransmozer@gmail.com>
2025-01-30 18:54:52 -0500

    TS refactors for ColourPicker component & snapshot updates

--- src/components/ColourPicker/index.tsx
+++ src/components/ColourPicker/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-interface ColourPickerProps {
+type ColourPickerProps = {
   colours: { [key: string]: string }
   selectedColour: string
   onColourSelect: (colour: string) => void
 }
 
-const ColourPicker: React.FC<ColourPickerProps> = ({
+const ColourPicker = ({
   colours,
   selectedColour,
   onColourSelect,
-}) => {
+}: ColourPickerProps) => {
   return (
     <div className="fixed bottom-6 right-6 flex gap-2">
       {Object.entries(colours).map(([name, colour]) => (
@@ -19,9 +19,7 @@ const ColourPicker: React.FC<ColourPickerProps> = ({
           onClick={() => onColourSelect(colour)}
           className={`w-8 h-8 rounded-full border-2 ${
             selectedColour === colour ? 'border-black' : 'border-gray-100'
-          }
-          hover:scale-110 transition-transform
-          hover:-translate-y-0.5 hover:shadow-lg hover:border-gray-500
+          } hover:scale-110 transition-transform hover:-translate-y-0.5 hover:shadow-lg hover:border-gray-500
           duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500`}
           style={{ backgroundColor: colour }}
           aria-label={`Select ${name} theme`}
